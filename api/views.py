bfc24dd4adffaa2b48ac1b63570b230c644b5aaa
RAVOTINA Kenny Judickael <judickaelkenny@gmail.com>
2025-08-06 17:44:39 +0300

    view list transaction

--- api/views.py
+++ api/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
-# Create your views here.
+from rest_framework import generics
+from .models import Transaction
+from .serializers import TransactionSerializer
+class TransactionListCreateView(generics.ListCreateAPIView):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
