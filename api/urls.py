ec887654e9eb2850cbd64923a1d528da6296421e
RAVOTINA Kenny Judickael <judickaelkenny@gmail.com>
2025-08-07 16:52:27 +0300

    update and delete trasaction

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,5 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('transactions/', views.TransactionListCreateView().as_view()),
+    path('transactions/<uuid:id>/', views.TransactionRetrieveUpdateDestroyView().as_view()),
 ]
