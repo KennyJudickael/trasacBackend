2971e3f1b206d756ce140de10a987cb3227ac9db
RAVOTINA Kenny Judickael <judickaelkenny@gmail.com>
2025-08-07 16:44:08 +0300

    create an amount

--- api/views.py
+++ api/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 from rest_framework import generics
 from .models import Transaction
 from .serializers import TransactionSerializer
+
+@method_decorator(csrf_exempt, name='dispatch')
 class TransactionListCreateView(generics.ListCreateAPIView):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
